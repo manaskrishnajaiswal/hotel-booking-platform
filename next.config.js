9e9d91da58975b2aaf643d19b61c65ebfff96f9d
manas5996 <manas5996@gmail.com>
2023-03-13 11:13:34 +0530

    Start ignoring next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    MONGO_URI:
-      "mongodb+srv://manas5996:Mkj5996@eplantdb.25bbqlq.mongodb.net/hotel-booking-platform?retryWrites=true&w=majority",
-  },
-};
-
-module.exports = nextConfig;
